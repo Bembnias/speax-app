606931e46ef6567250e8ec529ffaa6028dd77b0c
mtbienias <bieniasdevs@gmail.com>
2022-11-21 20:04:45 +0100

    Przygotowanie zmiennych do rejestracji, nadanie ID elementom graficznym

--- app/src/main/java/com/example/speax/RegisterActivity.java
+++ app/src/main/java/com/example/speax/RegisterActivity.java
@@ -1,10 +1,12 @@
 package com.example.speax;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -15,6 +17,7 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        // Przycisk odsylania do ekranu logowania
         toLoginButton = (TextView) findViewById(R.id.register_summary_label);
         toLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,6 +25,13 @@ public class RegisterActivity extends AppCompatActivity {
                 openLoginActivity();
             }
         });
+
+        // Tworzenie nowego konta użytkownika
+        final EditText email = findViewById(R.id.register_email_field);
+        final EditText name = findViewById(R.id.register_name_field);
+        final EditText surname = findViewById(R.id.register_surname_field);
+        final EditText password = findViewById(R.id.register_password_field);
+        
     }
 
     public void openLoginActivity() {
