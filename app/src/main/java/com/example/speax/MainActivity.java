ccee271c62f15271459fe15677e2bf1da163aeef
mtbienias <bieniasdevs@gmail.com>
2022-11-24 12:05:00 +0100

    Usunieto poprzednie zmiany, ktore powodowały błędy w kompilacji aplikacji

--- app/src/main/java/com/example/speax/MainActivity.java
+++ app/src/main/java/com/example/speax/MainActivity.java
@@ -5,55 +5,45 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
-    private String email;
-    private String name;
-    private String surname;
-    private RecyclerView messagesRecyclerView;
-    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://speax-app-default-rtdb.firebaseio.com");
+    private TextView toRegisterButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final CircleImageView userProfilePic = findViewById(R.id.user_profile_image);
-
-        messagesRecyclerView = findViewById(R.id.messages_recycler_view);
-
-        // Pobierz dane wyslane z reigsterActivity
-        email = getIntent().getStringExtra("email");
-        name = getIntent().getStringExtra("name");
-        surname = getIntent().getStringExtra("surname");
-
-        messagesRecyclerView.setHasFixedSize(true);
-        messagesRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        // Pobierz zdjecie profilowe z firebase
-        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+        // Przycisk odsylania do ekranu rejestracji
+        toRegisterButton = (TextView) findViewById(R.id.register_summary_label);
+        toRegisterButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                final String profileImageUrl = snapshot.child("users").child(email).child("profile_image").getValue(String.class);
-                userProfilePic.
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
+            public void onClick(View view) {
+                openRegisterActivity();
             }
         });
+    }
+
+    public void openRegisterActivity() {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/speax/RegisterActivity.java
+++ app/src/main/java/com/example/speax/RegisterActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -20,8 +21,6 @@ import com.google.firebase.database.ValueEventListener;
 public class RegisterActivity extends AppCompatActivity {
     private TextView toLoginButton;
 
-    DatabaseReference dbRef = FirebaseDatabase.getInstance().getReferenceFromUrl("https://speax-app-default-rtdb.firebaseio.com")
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,55 +35,6 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
 
-        // Tworzenie nowego konta użytkownika
-        final EditText email = findViewById(R.id.register_email_field);
-        final EditText name = findViewById(R.id.register_name_field);
-        final EditText surname = findViewById(R.id.register_surname_field);
-        final EditText password = findViewById(R.id.register_password_field);
-        final AppCompatButton registerConfirmBtn = findViewById(R.id.register_confirm_button);
-
-        registerConfirmBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final String nameTxt = name.getText().toString();
-                final String emailTxt = name.getText().toString();
-                final String surnameTxt = name.getText().toString();
-                final String passwordTxt = name.getText().toString();
-
-                if(nameTxt.isEmpty() || emailTxt.isEmpty() || surnameTxt.isEmpty() || passwordTxt.isEmpty()) {
-                    Toast.makeText(RegisterActivity.this, "Wszystkie pole muszą zostać uzupełnione!", Toast.LENGTH_SHORT).show();
-                } else {
-                    dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(@NonNull DataSnapshot snapshot) {
-
-                            if(snapshot.child("users").hasChild(emailTxt)) {
-                                Toast.makeText(RegisterActivity.this, "Taki email już istnieje!", Toast.LENGTH_SHORT).show();
-                            } else {
-                                dbRef.child("users").child(emailTxt).child("email").setValue(emailTxt);
-                                dbRef.child("users").child(emailTxt).child("name").setValue(nameTxt);
-                                dbRef.child("users").child(emailTxt).child("surname").setValue(surnameTxt);
-                                dbRef.child("users").child(emailTxt).child("password").setValue(passwordTxt);
-
-                                Toast.makeText(RegisterActivity.this, "Poprawnie zarejestrowano!", Toast.LENGTH_SHORT).show();
-
-                                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
-                                intent.putExtra("email", emailTxt);
-                                intent.putExtra("name", nameTxt);
-                                intent.putExtra("surname", surnameTxt);
-                                startActivity(intent);
-                                finish();
-                            }
-                        }
-
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError error) {
-
-                        }
-                    });
-                }
-            }
-        });
     }
 
     public void openLoginActivity() {
